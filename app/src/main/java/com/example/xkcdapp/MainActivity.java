8cbe44ffcefe3dac5b146caa30620459cff88ea6
IMadCauseIBad <Loppy788>
2019-05-01 12:44:56 -0500

    file

--- app/src/main/java/com/example/xkcdapp/MainActivity.java
+++ app/src/main/java/com/example/xkcdapp/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import org.json.JSONException;
 import org.json.JSONObject;
